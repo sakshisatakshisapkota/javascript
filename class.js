168bbf254f87a26afb0275fc2e575f16a519738b
sakshisatakshisapkota <sapkota.radha876@gmail.com>
2025-01-30 15:17:48 +0545

    javascript skillshikhya

---
+++ class.js
@@ -0,0 +1,5 @@
+// let a  = Number(prompt("Enter a first number"))
+// let b = Number( prompt ( "Enter a  second number") )
+//   console.log ("Addition", a+b );   
+
+console.log ("Radha")
---
+++ script.js
@@ -0,0 +1,23 @@
+console.log("hello world");
+ 
+let hobby="Programming";
+var age=25;
+const name ="Radha";
+hobby= "playing Games"
+//  alert ( "I am lesrning javascript");
+let user_name= prompt ( "what is your name ?")
+console.log (user_name);
+ let isActive = confirm ( "Are u a programming?")
+ console.log ( isActive);
+ if (isActive) {
+    console.log ( "you are  programming");
+ }else{
+    console.log (" you are not programmingssss")
+ }
+ let number1="20";
+ console.log ("before Conversion" ,number1+5);
+ console.log("Number Conversion", (number1));
+ let a= 10;
+ let b=5;
+ console.log ( "Addition" ,a+b);
+ 
